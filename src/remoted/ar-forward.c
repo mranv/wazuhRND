b4340b9c191aab6ffc156c6b0e07f31649b9e31f
mranv <iamanubhavgain@gmail.com>
2024-07-24 18:36:09 +0530

    revert: d35e42b34a841b96ccbe335ee300b302f302ad01

--- src/remoted/ar-forward.c
+++ src/remoted/ar-forward.c
@@ -1,82 +1,65 @@
+/* Copyright (C) 2015, Wazuh Inc.
+ * Copyright (C) 2009 Trend Micro Inc.
+ * All right reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
+
 #include <pthread.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/time.h>
 
 #include "shared.h"
 #include "remoted.h"
 #include "state.h"
 #include "os_net/os_net.h"
 
-#define LOG_FILE "/var/ossec/logs/ar_forward.log"
-#define LOG_BUFFER_SIZE 2048
 
-static FILE *log_fp = NULL;
-static char log_buffer[LOG_BUFFER_SIZE];
-
-// Efficient logger function
-static void log_message(const char *message) {
-    struct timeval tv;
-    struct tm *tm_info;
-
-    if (!log_fp) {
-        log_fp = fopen(LOG_FILE, "a");
-        if (!log_fp) {
-            merror("Unable to open log file: %s", LOG_FILE);
-            return;
-        }
-        setbuf(log_fp, NULL);  // Disable buffering
-    }
-
-    gettimeofday(&tv, NULL);
-    tm_info = localtime(&tv.tv_sec);
-
-    fprintf(log_fp, "[%04d-%02d-%02d %02d:%02d:%02d.%03ld] %s\n",
-            tm_info->tm_year + 1900, tm_info->tm_mon + 1, tm_info->tm_mday,
-            tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec,
-            tv.tv_usec / 1000, message);
-}
-
+/* Start of a new thread. Only returns on unrecoverable errors. */
 void *AR_Forward(__attribute__((unused)) void *arg)
 {
     int arq = 0;
     int ar_location = 0;
-    const char *path = ARQUEUE;
+    const char * path = ARQUEUE;
     char *msg_to_send;
+    os_calloc(OS_MAXSTR, sizeof(char), msg_to_send);
     char *msg;
+    os_calloc(OS_MAXSTR, sizeof(char), msg);
     char *ar_agent_id = NULL;
     char *tmp_str = NULL;
 
-    os_calloc(OS_MAXSTR, sizeof(char), msg_to_send);
-    os_calloc(OS_MAXSTR, sizeof(char), msg);
-
+    /* Create the unix queue */
     if ((arq = StartMQ(path, READ, 0)) < 0) {
-        snprintf(log_buffer, LOG_BUFFER_SIZE, "Error creating unix queue: %s", strerror(errno));
-        log_message(log_buffer);
         merror_exit(QUEUE_ERROR, path, strerror(errno));
     }
 
-    log_message("AR_Forward started");
-
+    /* Daemon loop */
     while (1) {
         if (OS_RecvUnix(arq, OS_MAXSTR - 1, msg)) {
+
+            mdebug2("Active response request received: %s", msg);
+
+            /* Always zero the location */
             ar_location = 0;
 
+            /* Location */
             tmp_str = strchr(msg, ')');
             if (!tmp_str) {
-                log_message("Invalid message format: missing ')'");
+                mwarn(EXECD_INV_MSG, msg);
                 continue;
             }
             tmp_str += 2;
 
+            /* Source IP */
             tmp_str = strchr(tmp_str, ']');
             if (!tmp_str) {
-                log_message("Invalid message format: missing ']'");
+                mwarn(EXECD_INV_MSG, msg);
                 continue;
             }
             tmp_str += 2;
 
+            /* AR location */
             if (*tmp_str == ALL_AGENTS_C) {
                 ar_location |= ALL_AGENTS;
             }
@@ -92,23 +75,35 @@ void *AR_Forward(__attribute__((unused)) void *arg)
             }
             tmp_str += 2;
 
+            /* Extract the agent id */
             ar_agent_id = tmp_str;
             tmp_str = strchr(tmp_str, ' ');
             if (!tmp_str) {
-                log_message("Invalid message format: missing space after agent ID");
+                mwarn(EXECD_INV_MSG, msg);
                 continue;
             }
             *tmp_str = '\0';
             tmp_str++;
 
+            /* Create the new message */
             if (ar_location & NO_AR_MSG) {
-                snprintf(msg_to_send, OS_MAXSTR, "%s%s", CONTROL_HEADER, tmp_str);
+                snprintf(msg_to_send, OS_MAXSTR, "%s%s",
+                         CONTROL_HEADER,
+                         tmp_str);
             } else {
-                snprintf(msg_to_send, OS_MAXSTR, "%s%s%s", CONTROL_HEADER, EXECD_HEADER, tmp_str);
+                snprintf(msg_to_send, OS_MAXSTR, "%s%s%s",
+                         CONTROL_HEADER,
+                         EXECD_HEADER,
+                         tmp_str);
             }
 
+            mdebug2("Active response sent: %s", msg_to_send);
+
+            /* Send to ALL agents */
             if (ar_location & ALL_AGENTS) {
                 char agent_id[KEYSIZE + 1] = "";
+
+                /* Lock use of keys */
                 key_lock_read();
 
                 for (unsigned int i = 0; i < keys.keysize; i++) {
@@ -117,35 +112,20 @@ void *AR_Forward(__attribute__((unused)) void *arg)
                         key_unlock();
                         if (send_msg(agent_id, msg_to_send, -1) >= 0) {
                             rem_inc_send_ar(agent_id);
-                            snprintf(log_buffer, LOG_BUFFER_SIZE, "AR sent to agent %s", agent_id);
-                            log_message(log_buffer);
-                        } else {
-                            snprintf(log_buffer, LOG_BUFFER_SIZE, "Failed to send AR to agent %s", agent_id);
-                            log_message(log_buffer);
                         }
                         key_lock_read();
                     }
                 }
 
                 key_unlock();
-            } else if (ar_location & (REMOTE_AGENT | SPECIFIC_AGENT)) {
+            }
+
+            /* Send to the remote agent that generated the event or to a pre-defined agent */
+            else if (ar_location & (REMOTE_AGENT | SPECIFIC_AGENT)) {
                 if (send_msg(ar_agent_id, msg_to_send, -1) >= 0) {
                     rem_inc_send_ar(ar_agent_id);
-                    snprintf(log_buffer, LOG_BUFFER_SIZE, "AR sent to agent %s", ar_agent_id);
-                    log_message(log_buffer);
-                } else {
-                    snprintf(log_buffer, LOG_BUFFER_SIZE, "Failed to send AR to agent %s", ar_agent_id);
-                    log_message(log_buffer);
                 }
             }
         }
     }
-
-    // This part will never be reached
-    os_free(msg_to_send);
-    os_free(msg);
-    if (log_fp) {
-        fclose(log_fp);
-    }
-    return NULL;
 }
--- src/remoted/cfga-forward.c
+++ src/remoted/cfga-forward.c
@@ -1,86 +1,59 @@
+/* Copyright (C) 2015, Wazuh Inc.
+ * Copyright (C) 2009 Trend Micro Inc.
+ * All right reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
+
 #include <pthread.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/time.h>
 
 #include "shared.h"
 #include "remoted.h"
 #include "state.h"
 #include "os_net/os_net.h"
 
-#define LOG_FILE "/var/ossec/logs/scfga_forward.log"
-#define LOG_BUFFER_SIZE 2048
 
-static FILE *log_fp = NULL;
-static char log_buffer[LOG_BUFFER_SIZE];
-
-// Efficient logger function
-static void log_message(const char *message) {
-    struct timeval tv;
-    struct tm *tm_info;
-
-    if (!log_fp) {
-        log_fp = fopen(LOG_FILE, "a");
-        if (!log_fp) {
-            merror("Unable to open log file: %s", LOG_FILE);
-            return;
-        }
-        setbuf(log_fp, NULL);  // Disable buffering
-    }
-
-    gettimeofday(&tv, NULL);
-    tm_info = localtime(&tv.tv_sec);
-
-    fprintf(log_fp, "[%04d-%02d-%02d %02d:%02d:%02d.%03ld] %s\n",
-            tm_info->tm_year + 1900, tm_info->tm_mon + 1, tm_info->tm_mday,
-            tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec,
-            tv.tv_usec / 1000, message);
-}
-
+/* Start of a new thread. Only returns on unrecoverable errors. */
 void *SCFGA_Forward(__attribute__((unused)) void *arg)
 {
     int cfgarq = 0;
     char *agent_id;
-    const char *path = CFGARQUEUE;
+    const char * path = CFGARQUEUE;
+
     char msg[OS_SIZE_4096 + 1];
 
+    /* Create the unix queue */
     if ((cfgarq = StartMQ(path, READ, 0)) < 0) {
-        snprintf(log_buffer, LOG_BUFFER_SIZE, "Error creating unix queue: %s", strerror(errno));
-        log_message(log_buffer);
         merror_exit(QUEUE_ERROR, path, strerror(errno));
     }
 
-    log_message("SCFGA_Forward started");
+    memset(msg, '\0', OS_SIZE_4096 + 1);
 
+    /* Daemon loop */
     while (1) {
         if (OS_RecvUnix(cfgarq, OS_SIZE_4096, msg)) {
+
             agent_id = msg;
-            char *msg_dump = strchr(msg, ':');
 
-            if (msg_dump) {
+            char *msg_dump = strchr(msg,':');
+
+            if(msg_dump) {
                 *msg_dump++ = '\0';
+            } else {
+                continue;
+            }
 
-                if (strncmp(msg_dump, CFGA_DB_DUMP, strlen(CFGA_DB_DUMP)) == 0) {
-                    char final_msg[OS_SIZE_4096 + 1] = {0};
-                    snprintf(final_msg, OS_SIZE_4096, "%s%s", CONTROL_HEADER, msg_dump);
+            if(strncmp(msg_dump,CFGA_DB_DUMP,strlen(CFGA_DB_DUMP)) == 0) {
+                char final_msg[OS_SIZE_4096 + 1] = {0};
 
-                    if (send_msg(agent_id, final_msg, -1) >= 0) {
-                        rem_inc_send_cfga(agent_id);
-                        snprintf(log_buffer, LOG_BUFFER_SIZE, "Message sent to agent %s", agent_id);
-                        log_message(log_buffer);
-                    } else {
-                        snprintf(log_buffer, LOG_BUFFER_SIZE, "Failed to send message to agent %s", agent_id);
-                        log_message(log_buffer);
-                    }
+                snprintf(final_msg, OS_SIZE_4096, "%s%s", CONTROL_HEADER, msg_dump);
+                if (send_msg(agent_id, final_msg, -1) >= 0) {
+                    rem_inc_send_cfga(agent_id);
                 }
             }
         }
     }
-
-    // This part will never be reached
-    if (log_fp) {
-        fclose(log_fp);
-    }
-    return NULL;
 }
