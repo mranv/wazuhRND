bbb28db7854daa7352e832e3e0af6d951996a92f
mranv <iamanubhavgain@gmail.com>
2024-07-25 18:13:43 +0530

    remove: logger in c code of ar-forwarder and cfga forwarder

--- src/remoted/ar-forward.c
+++ src/remoted/ar-forward.c
@@ -9,17 +9,15 @@
  */
 
 #include <pthread.h>
+
 #include "shared.h"
 #include "remoted.h"
 #include "state.h"
-#include "wazuh_modules/log_function.h"
 #include "os_net/os_net.h"
 
 /* Start of a new thread. Only returns on unrecoverable errors. */
 void *AR_Forward(__attribute__((unused)) void *arg)
 {
-    ar_log_function(__FUNCTION__, "Thread started");
-
     int arq = 0;
     int ar_location = 0;
     const char *path = ARQUEUE;
@@ -33,129 +31,120 @@ void *AR_Forward(__attribute__((unused)) void *arg)
     /* Create the unix queue */
     if ((arq = StartMQ(path, READ, 0)) < 0)
     {
-        ar_log_function(__FUNCTION__, "Failed to create unix queue: %s. Path: %s", strerror(errno), path);
         merror_exit(QUEUE_ERROR, path, strerror(errno));
     }
-
-    ar_log_function(__FUNCTION__, "Unix queue created successfully. Path: %s", path);
 
     /* Daemon loop */
     while (1)
     {
-        if (OS_RecvUnix(arq, OS_MAXSTR - 1, msg) <= 0)
+        if (OS_RecvUnix(arq, OS_MAXSTR - 1, msg))
         {
-            continue;
-        }
 
-        /* Always zero the location */
-        ar_location = 0;
+            mdebug2("Active response request received: %s", msg);
 
-        /* Location */
-        tmp_str = strchr(msg, ')');
-        if (!tmp_str)
-        {
-            mwarn(EXECD_INV_MSG, msg);
-            continue;
-        }
-        tmp_str += 2;
+            /* Always zero the location */
+            ar_location = 0;
 
-        /* Source IP */
-        tmp_str = strchr(tmp_str, ']');
-        if (!tmp_str)
-        {
-            mwarn(EXECD_INV_MSG, msg);
-            continue;
-        }
-        tmp_str += 2;
-
-        /* AR location */
-        if (*tmp_str == ALL_AGENTS_C)
-        {
-            ar_location |= ALL_AGENTS;
-        }
-        tmp_str++;
-        if (*tmp_str == REMOTE_AGENT_C)
-        {
-            ar_location |= REMOTE_AGENT;
-        }
-        else if (*tmp_str == NO_AR_C)
-        {
-            ar_location |= NO_AR_MSG;
-        }
-        tmp_str++;
-        if (*tmp_str == SPECIFIC_AGENT_C)
-        {
-            ar_location |= SPECIFIC_AGENT;
-        }
-        tmp_str += 2;
-
-        /* Extract the agent id */
-        ar_agent_id = tmp_str;
-        tmp_str = strchr(tmp_str, ' ');
-        if (!tmp_str)
-        {
-            mwarn(EXECD_INV_MSG, msg);
-            continue;
-        }
-        *tmp_str = '\0';
-        tmp_str++;
-
-        /* Create the new message */
-        if (ar_location & NO_AR_MSG)
-        {
-            snprintf(msg_to_send, OS_MAXSTR, "%s%s",
-                     CONTROL_HEADER,
-                     tmp_str);
-        }
-        else
-        {
-            snprintf(msg_to_send, OS_MAXSTR, "%s%s%s",
-                     CONTROL_HEADER,
-                     EXECD_HEADER,
-                     tmp_str);
-        }
-
-        /* Send to ALL agents */
-        if (ar_location & ALL_AGENTS)
-        {
-            char agent_id[KEYSIZE + 1] = "";
-
-            /* Lock use of keys */
-            key_lock_read();
-
-            for (unsigned int i = 0; i < keys.keysize; i++)
+            /* Location */
+            tmp_str = strchr(msg, ')');
+            if (!tmp_str)
             {
-                if (keys.keyentries[i]->rcvd >= (time(0) - logr.global.agents_disconnection_time))
-                {
-                    strncpy(agent_id, keys.keyentries[i]->id, KEYSIZE);
-                    key_unlock();
-                    if (send_msg(agent_id, msg_to_send, -1) >= 0)
-                    {
-                        rem_inc_send_ar(agent_id);
-                    }
-                    if (OS_RecvUnix(arq, OS_MAXSTR - 1, msg) <= 0)
-                    {
-                        continue;
-                    }
-                    key_lock_read();
-                }
+                mwarn(EXECD_INV_MSG, msg);
+                continue;
+            }
+            tmp_str += 2;
+
+            /* Source IP */
+            tmp_str = strchr(tmp_str, ']');
+            if (!tmp_str)
+            {
+                mwarn(EXECD_INV_MSG, msg);
+                continue;
+            }
+            tmp_str += 2;
+
+            /* AR location */
+            if (*tmp_str == ALL_AGENTS_C)
+            {
+                ar_location |= ALL_AGENTS;
+            }
+            tmp_str++;
+            if (*tmp_str == REMOTE_AGENT_C)
+            {
+                ar_location |= REMOTE_AGENT;
+            }
+            else if (*tmp_str == NO_AR_C)
+            {
+                ar_location |= NO_AR_MSG;
+            }
+            tmp_str++;
+            if (*tmp_str == SPECIFIC_AGENT_C)
+            {
+                ar_location |= SPECIFIC_AGENT;
+            }
+            tmp_str += 2;
+
+            /* Extract the agent id */
+            ar_agent_id = tmp_str;
+            tmp_str = strchr(tmp_str, ' ');
+            if (!tmp_str)
+            {
+                mwarn(EXECD_INV_MSG, msg);
+                continue;
+            }
+            *tmp_str = '\0';
+            tmp_str++;
+
+            /* Create the new message */
+            if (ar_location & NO_AR_MSG)
+            {
+                snprintf(msg_to_send, OS_MAXSTR, "%s%s",
+                         CONTROL_HEADER,
+                         tmp_str);
+            }
+            else
+            {
+                snprintf(msg_to_send, OS_MAXSTR, "%s%s%s",
+                         CONTROL_HEADER,
+                         EXECD_HEADER,
+                         tmp_str);
             }
 
-            key_unlock();
-        }
+            mdebug2("Active response sent: %s", msg_to_send);
 
-        /* Send to the remote agent that generated the event or to a pre-defined agent */
-        else if (ar_location & (REMOTE_AGENT | SPECIFIC_AGENT))
-        {
-            if (send_msg(ar_agent_id, msg_to_send, -1) >= 0)
+            /* Send to ALL agents */
+            if (ar_location & ALL_AGENTS)
             {
-                rem_inc_send_ar(ar_agent_id);
+                char agent_id[KEYSIZE + 1] = "";
+
+                /* Lock use of keys */
+                key_lock_read();
+
+                for (unsigned int i = 0; i < keys.keysize; i++)
+                {
+                    if (keys.keyentries[i]->rcvd >= (time(0) - logr.global.agents_disconnection_time))
+                    {
+                        strncpy(agent_id, keys.keyentries[i]->id, KEYSIZE);
+                        key_unlock();
+                        if (send_msg(agent_id, msg_to_send, -1) >= 0)
+                        {
+                            rem_inc_send_ar(agent_id);
+                        }
+                        key_lock_read();
+                    }
+                }
+
+                key_unlock();
+            }
+
+            /* Send to the remote agent that generated the event or to a pre-defined agent */
+            else if (ar_location & (REMOTE_AGENT | SPECIFIC_AGENT))
+            {
+                if (send_msg(ar_agent_id, msg_to_send, -1) >= 0)
+                {
+                    rem_inc_send_ar(ar_agent_id);
+                }
             }
         }
     }
-
-    // This point should never be reached
-    free(msg_to_send);
-    free(msg);
-    return NULL;
 }
--- src/remoted/cfga-forward.c
+++ src/remoted/cfga-forward.c
@@ -9,7 +9,7 @@
  */
 
 #include <pthread.h>
-#include "wazuh_modules/log_function.h"
+
 #include "shared.h"
 #include "remoted.h"
 #include "state.h"
@@ -18,8 +18,6 @@
 /* Start of a new thread. Only returns on unrecoverable errors. */
 void *SCFGA_Forward(__attribute__((unused)) void *arg)
 {
-    scfga_log_function(__func__, "Thread started");
-
     int cfgarq = 0;
     char *agent_id;
     const char *path = CFGARQUEUE;
@@ -29,21 +27,16 @@ void *SCFGA_Forward(__attribute__((unused)) void *arg)
     /* Create the unix queue */
     if ((cfgarq = StartMQ(path, READ, 0)) < 0)
     {
-        scfga_log_function(__func__, "Failed to create unix queue: %s. Path: %s", strerror(errno), path);
         merror_exit(QUEUE_ERROR, path, strerror(errno));
     }
-
-    scfga_log_function(__func__, "Unix queue created successfully. Path: %s", path);
 
     memset(msg, '\0', OS_SIZE_4096 + 1);
 
     /* Daemon loop */
     while (1)
     {
-        scfga_log_function(__func__, "Waiting for message on queue");
         if (OS_RecvUnix(cfgarq, OS_SIZE_4096, msg))
         {
-            scfga_log_function(__func__, "Message received: %s", msg);
 
             agent_id = msg;
 
@@ -52,47 +45,22 @@ void *SCFGA_Forward(__attribute__((unused)) void *arg)
             if (msg_dump)
             {
                 *msg_dump++ = '\0';
-                scfga_log_function(__func__, "Agent ID: %s, Message: %s", agent_id, msg_dump);
             }
             else
             {
-                scfga_log_function(__func__, "Invalid message format (missing ':'): %s", msg);
                 continue;
             }
 
             if (strncmp(msg_dump, CFGA_DB_DUMP, strlen(CFGA_DB_DUMP)) == 0)
             {
-                scfga_log_function(__func__, "CFGA_DB_DUMP message detected");
                 char final_msg[OS_SIZE_4096 + 1] = {0};
 
                 snprintf(final_msg, OS_SIZE_4096, "%s%s", CONTROL_HEADER, msg_dump);
-                scfga_log_function(__func__, "Final message prepared: %s", final_msg);
-
                 if (send_msg(agent_id, final_msg, -1) >= 0)
                 {
-                    scfga_log_function(__func__, "Message sent successfully to agent: %s", agent_id);
                     rem_inc_send_cfga(agent_id);
                 }
-                else
-                {
-                    scfga_log_function(__func__, "Failed to send message to agent: %s", agent_id);
-                }
-            }
-            else
-            {
-                scfga_log_function(__func__, "Unexpected message type: %s", msg_dump);
             }
         }
-        else
-        {
-            scfga_log_function(__func__, "No message received from queue");
-        }
-
-        // Reset msg buffer for next iteration
-        memset(msg, '\0', OS_SIZE_4096 + 1);
     }
-
-    // This point should never be reached
-    scfga_log_function(__func__, "Thread exiting unexpectedly");
-    return NULL;
 }
